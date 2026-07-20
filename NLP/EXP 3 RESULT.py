86da0816a0b7d9466e18e97322b71be05fbb5445
ajay reddy <pothulaajayreddy@gmail.com>
2026-07-20 11:23:39 +0530

    Add files via upload

---
+++ NLP/EXP 3 RESULT.py
@@ -0,0 +1,10 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+============ RESTART: C:/Users/ajayk/OneDrive/Documents/NLP exp 3.py ===========
+[nltk_data] Downloading package wordnet to
+[nltk_data]     C:\Users\ajayk\AppData\Roaming\nltk_data...
+Enter a word: running
+Original Word: running
+Stemmed Word: run
+Lemmatized Word: running
